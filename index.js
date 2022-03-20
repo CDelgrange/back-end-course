162bba3a22cdb487c579f430ac9ff034d539152c
Charlely Delgrange <charlely.delgrange@gmail.com>
2022-03-20 11:40:09 +0100

    feat: add postgres db connecion + auto restart serv

--- index.js
+++ index.js
@@ -2,10 +2,30 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', function(req, res) {
-  res.send('Hello World!')
+const { Sequelize, Model, DataTypes } = require('sequelize');
+const sequelize = new Sequelize(process.env.DB_URL);
+
+class User extends Model {}
+User.init({
+  username: DataTypes.STRING,
+  birthday: DataTypes.DATE,
+}, { sequelize, tableName: 'user' });
+
+app.get('/', async function(req, res) {
+  await sequelize.sync();
+
+  const jane = await User.create({
+    username: 'janedoe',
+    birthday: new Date(1980, 6, 20),
+  });
+  console.log(jane.toJSON());
+
+  const x = await User.findAll();
+  console.log(x.map(u => u.username));
+
+  res.send('Hello World!');
 });
 
 app.listen(port, function() {
-  console.log(`Example app listening on port ${port}!`)
+  console.log(`Example app listening on port ${port}!`);
 });
