d19c7e500ac8c077775c77d04cc20a11712291b1
Charlely Delgrange <charlely.delgrange@gmail.com>
2022-03-24 08:06:34 +0100

    remove be container in compose

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
+require('dotenv').config()
 const express = require('express');
 const app = express();
 const port = 3000;
 
+const getDbUrl = () => {
+  const pgHost = process.env.POSTGRES_HOST;
+  const pgDb = process.env.POSTGRES_DB;
+  const pgUser = process.env.POSTGRES_USER;
+  const pgPassword = process.env.POSTGRES_PASSWORD;
+  const pgPort = process.env.POSTGRES_PORT;
+
+  return `postgres://${pgUser}:${pgPassword}@${pgHost}:${pgPort}/${pgDb}`;
+}
+
 const { Sequelize, Model, DataTypes } = require('sequelize');
-const sequelize = new Sequelize(process.env.DB_URL);
+const sequelize = new Sequelize(getDbUrl());
 
 class User extends Model {}
 User.init({
@@ -25,6 +36,13 @@ app.get('/', async function(req, res) {
 
   res.send('Hello World!');
 });
+
+app.put('/users/:id', (req, res) => {
+  console.log(`We want to update the user with id: ${req.params.id}`);
+  console.log(`And the data required for the update will be:\n${JSON.stringify(req.body, null, 2)}`);
+
+  res.send('Update will be fine');
+})
 
 app.listen(port, function() {
   console.log(`Example app listening on port ${port}!`);
